287e93986a2c164b90d4fa55d0e79f0556d509c8
Ivan belonozhkin <hezhakome@mail.ru>
2020-12-23 19:35:06 +0300

    homework 1

--- projects/dz functions/index.js
+++ projects/dz functions/index.js
@@ -12,7 +12,9 @@
 
  Другими словами: функция должна возвращать в неизменном виде то, что поступает ей на вход
  */
-function returnFirstArgument() {}
+function returnFirstArgument(Argument) {
+  return Argument;
+}
 
 /*
  Задание 2:
@@ -28,7 +30,11 @@ function returnFirstArgument() {}
  Пример:
    sumWithDefaults(10) вернет 110
  */
-function sumWithDefaults(a, b) {}
+
+function sumWithDefaults(a, b = 100) {
+  const sum = a + b;
+  return sum;
+}
 
 /*
  Задание 3:
@@ -38,7 +44,9 @@ function sumWithDefaults(a, b) {}
  Пример:
    returnFnResult(() => 'привет') вернет 'привет'
  */
-function returnFnResult(fn) {}
+function returnFnResult(fn) {
+  return fn();
+}
 
 /*
  Задание 4:
@@ -53,7 +61,11 @@ function returnFnResult(fn) {}
    console.log(f()); // выведет 12
    console.log(f()); // выведет 13
  */
-function returnCounter(number) {}
+function returnCounter(number = 0) {
+  return function () {
+    return ++number;
+  };
+}
 
 /*
  Задание 5 *:
@@ -64,7 +76,9 @@ function returnCounter(number) {}
  Пример:
    returnArgumentsArray(1, 2, 3) вернет [1, 2, 3]
  */
-function returnArgumentsArray() {}
+function returnArgumentsArray() {
+  return [...arguments];
+}
 
 /*
  Задание 6 *:
